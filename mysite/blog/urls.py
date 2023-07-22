73dd19c14653351f6e193fe754016c4bfeb3dec1
bren0r <nuriomonov1@mail.ru>
2023-07-22 20:21:33 +0300

    renamed path

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^$', views.post_list, name='post_list'),
     path(r'^(?p<year>\d{4}/(?P<month>\d{2}/(?p<day>\d{2}/'
          r'(?P<post>[-\w]+/$',
          views.post_detail,
