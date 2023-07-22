85bd6973941441699a08279d542b656719510004
bren0r <nuriomonov1@mail.ru>
2023-07-22 19:37:53 +0300

    modify class PostAdmin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -4,6 +4,12 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'published', 'status')
+    list_filter = ('status', 'created', 'published', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title', )}
+    raw_id_fields = ('author', )
+    date_hierarchy = 'published'
+    ordering = ['status', 'published']
 
 
 admin.site.register(Post, PostAdmin)
